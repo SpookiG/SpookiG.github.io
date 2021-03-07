47d7e693e92edef825a71128abdda1b0296eb2cd
SpookiG <laycockmichael@yahoo.co.uk>
2021-03-07 22:37:25 +0000

    Filter buttons now work!

--- games/gameLoad.js
+++ games/gameLoad.js
@@ -1,5 +1,12 @@
+var frameHeightLookup = new Object();
+
 document.addEventListener("DOMContentLoaded", function(){
     let frames = document.getElementsByTagName("iframe");
+    
+    Array.prototype.forEach.call(frames, frame => {
+        let frameKey = frame.getAttribute("class");
+        frameHeightLookup[frameKey] = "0px";
+    });
 
     //alert(frames[0].style.height.toString());
     
@@ -20,9 +27,46 @@ document.addEventListener("DOMContentLoaded", function(){
 }*/
 
 window.onmessage = function(mess) {
+    let frameKey = mess.data.id;
+    frameHeightLookup[frameKey] = mess.data.height + "px";
+
     let frames = document.getElementsByClassName(mess.data.id);
     Array.prototype.forEach.call(frames, frame => {
-        frame.style.height = mess.data.height + "px";
+        frame.style.height = frameHeightLookup[frameKey];
     });
     
 }
+
+
+
+
+function hideFrame(frame) {
+    frame.style.height = "0px";
+}
+
+function showFrame(frame) {
+    // height = iframe content size
+
+    if (!frame.hasAttribute("src"))
+    {
+        let newSrc = "games/" + frame.getAttribute("class") + ".html";
+        frame.setAttribute("src", newSrc);
+    }
+    
+    let frameKey = frame.getAttribute("class");
+    frame.style.height = frameHeightLookup[frameKey];
+}
+
+
+// todo:
+/*
+
+1. change message send * to specific window
+2. load iframe and check tags to determine whether to hide ("Highlights-Programming" "Good-Art_Design")
+3. use this on current default page
+4. buttons to select goodness and area of work
+5. Actually make the info pages
+
+
+
+*/
