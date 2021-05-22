4a9a16794c0a747c1022f1f3d2a44c2f0e95cc47
SpookiG <laycockmichael@yahoo.co.uk>
2021-05-22 23:15:13 +0100

    Message now only posted to parent window for security practice

--- games/gameNotify.js
+++ games/gameNotify.js
@@ -4,5 +4,15 @@ window.onload = function() {
     let path = window.location.pathname;
     let page = path.split("/").pop().split(".")[0];
     data.id = page;
-    parent.postMessage(data,"*"); 
+
+    //origin = "https://spookig.github.io/games.html";
+    origin = window.parent.location;
+    if (window.location.protocol == "file:")
+    {
+        origin = "*";
+    }
+
+    console.log("sent message to: " + origin);
+
+    parent.postMessage(data, origin); 
 }
