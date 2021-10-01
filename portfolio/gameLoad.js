9eecfe1133ff134a206910856f23eee28adec2cc
SpookiG <laycockmichael@yahoo.co.uk>
2021-10-01 03:42:11 +0100

    Changed portfolio to focus more on programming
    It seems like showing everything you do and everything you're passionate about comes off badly, like you don't know what you're doing, so I've focused it more

---
+++ portfolio/gameLoad.js
@@ -0,0 +1,221 @@
+var frameHeightLookup;
+var frameActive;
+var qualitySelection;
+var categorySelection;
+var loaded = false;
+
+document.addEventListener("DOMContentLoaded", function(){
+    frameHeightLookup = new Object();
+    frameActive = new Object();
+    let frames = document.getElementsByTagName("iframe");
+    
+    Array.prototype.forEach.call(frames, function(frame) {
+        let frameKey = frame.id;
+        frameActive[frameKey] = true;
+    });
+
+    qualitySelection = null;
+    categorySelection = new Set();
+    updateQualitySelection();
+    updateCategorySelection();
+    loadSelections();
+
+    //alert(frames[0].style.height.toString());
+    loaded = true;
+    
+});
+
+/*window.onload = function() {
+    let frames = document.getElementsByTagName("iframe");
+
+    console.log(parent.document.body.clientHeight);
+    console.log(document.documentElement.scrollHeight);
+}*/
+
+/*function resizeHeight(frame) {
+    console.log(frame.style.height);
+    console.log(frame.contentWindow.height);
+
+    console.log(frame.document.height);
+}*/
+
+window.onmessage = function(mess) {
+    if (mess.data.type === "pageData") {
+        let frameKey = mess.data.id;
+        frameHeightLookup[frameKey] = mess.data.height + "px";
+
+        adjustFrameSize(frameKey);
+    } else if (mess.data.type === "expandImage") {
+        // copying over function displayImage from gameNotify.js
+
+        let modal = document.getElementById("myModal");
+        let modalImg = document.getElementById("modalImage");
+        let captionText = document.getElementById("caption");
+
+        modal.onclick = hideImage;
+        modal.style.display = "block";
+        modalImg.src = mess.data.src;
+        captionText.innerHTML = mess.data.alt;
+    }
+}
+
+
+function adjustFrameSize(frameKey) {
+    if (frameActive[frameKey]) {
+        if (loaded) {
+            let frame = document.getElementById(frameKey);
+            frame.style.height = frameHeightLookup[frameKey];
+        } else {
+            console.log("yo");
+            setTimeout(function() { adjustFrameSize(frameKey) }, 50);
+        }
+    }
+}
+
+
+function hideImage() {
+    let modal = document.getElementById("myModal");
+    modal.style.display = "none";
+    modal.onclick = null;
+}
+
+
+
+
+function hideFrame(frame) {
+    frame.style.height = "0px";
+    frameActive[frame.id] = false;
+}
+
+function showFrame(frame) {
+    // height = iframe content size
+    if (frame.src === "") {
+        let srcDirectory = window.location.pathname.split("/").pop().split(".")[0]; // maybe move somewhere else later for efficiency
+        frame.src = srcDirectory + "/" + frame.id + ".html";
+    }
+    
+    let frameKey = frame.id;
+    frame.style.height = frameHeightLookup[frameKey];
+    frameActive[frameKey] = true;
+}
+
+
+// quality selections => single variable
+// category selections => array / set
+
+
+
+
+function qualitySelect() {
+    updateQualitySelection();
+    loadSelections();
+}
+
+function categorySelect() {
+    updateCategorySelection();
+    loadSelections();
+}
+
+
+
+
+
+function updateQualitySelection() {
+    // build up a set containing the current form selection
+    let qualityForm = document.getElementById("quality-banner").getElementsByTagName("input");
+    let formSelection = new Set();
+    Array.prototype.forEach.call(qualityForm, function(item) {
+        if (item.checked) {
+            formSelection.add(item.parentNode.innerText);
+        }
+    });
+
+    // remove current selected and check & set if a new selected has been made (for context, only one quality selection can be selected at a time)
+    formSelection.delete(qualitySelection);
+    Array.prototype.forEach.call(qualityForm, function(item) {                           // Have to do this roundabout check because ie doesn't support sets very much
+        if (item.checked && formSelection.has(item.parentNode.innerText)) {
+            qualitySelection = item.parentNode.innerText;
+        }
+    });
+
+    if (qualitySelection === null) {
+        // shouldn't ever be here but let's just catch this and go
+        qualitySelection = qualityForm[0].parentNode.innerText;
+    }
+
+    // apply changes back to form
+    Array.prototype.forEach.call(qualityForm, function(item) {
+        if (item.parentNode.innerText === qualitySelection) {
+            item.checked = true;
+        } else {
+            item.checked = false;
+        }
+    });
+}
+
+function updateCategorySelection() {
+    // build up a set containing the current form selection
+    let categoryForm = document.getElementById("category-banner").getElementsByTagName("input");
+    let formSelection = new Set();
+    Array.prototype.forEach.call(categoryForm, function(item) {
+        if (item.checked) {
+            formSelection.add(item.parentNode.innerText);
+        }
+    });
+    categorySelection = formSelection;
+}
+
+function loadSelections() {
+    let frames = document.getElementById("frames").getElementsByTagName("iframe");
+    Array.prototype.forEach.call(frames, function(frame) {
+        // get 
+        let highlightedCategories = frame.getAttribute('data-highlight').split(" ");
+        let goodCategories = frame.getAttribute('data-good').split(" ");
+        let visible = false;
+
+        if (qualitySelection === "archive") {
+            visible = true;
+        } else {
+            if (qualitySelection === "good work") {
+                Array.prototype.forEach.call(goodCategories, function(category) {
+                    if (categorySelection.has(category)) {
+                        visible = true;
+                    }
+                });
+            }
+
+            // no need for a qualitySelection === highlights check here because highlights are also good work
+            Array.prototype.forEach.call(highlightedCategories, function(category) {
+                if (categorySelection.has(category)) {
+                    visible = true;
+                }
+            });
+        }
+        
+        if (visible) {
+            showFrame(frame);
+        } else {
+            hideFrame(frame);
+        }
+    });
+}
+
+
+
+
+// todo:
+/*
+
+5.6 add all images to iframes
+    art
+    design
+    programming
+5.7 lazy load iframes? Depends how slow games page ends up
+6. Add an other hobbies segment to my website (linking to photography, shaders, etc)
+7. Big website summary
+8. Add my name to that summary
+
+
+
+
+*/
---
+++ portfolio/gameNotify.js
@@ -0,0 +1,158 @@
+var height = 0;
+var sendResizeMessage = true;
+var collapsableCollapsed = true;
+
+window.onload = function() {
+    // attach onclick funtionality to collapsable content and expandable images here so it isn't needed in the html
+    let collapsables = document.getElementsByClassName("collapsible");
+    Array.prototype.forEach.call(collapsables, function(collapsable) {
+        collapsable.onclick = function() { collapse(collapsable); };
+    });
+
+
+    // change webpage CSS depending on if in an iframe or not
+    if (window === window.parent) {
+        let page = document.getElementById("page");
+        page.classList.add("page");
+        let content = document.getElementById("content");
+        content.classList.add("content");
+
+        document.body.classList.remove("iframe-colour");
+
+        let expandables = document.getElementsByClassName("clickable-image");
+        Array.prototype.forEach.call(expandables, function(expandable) {
+            expandable.onclick = function() { displayImage(expandable); };
+        });
+
+        Array.prototype.forEach.call(collapsables, function(collapsable) {
+            collapse(collapsable);
+        });
+
+    } else {
+        let page = document.getElementById("page");
+        page.classList.remove("page");
+        let content = document.getElementById("content");
+        content.classList.remove("content");
+
+        content.insertAdjacentHTML('afterbegin', '</br></br></br><div class="frame-separator"></div></br></br>');
+
+        document.body.classList.add("iframe-colour");
+
+        let expandables = document.getElementsByClassName("clickable-image");
+        Array.prototype.forEach.call(expandables, function(expandable) {
+            expandable.onclick = function() { sendImageToParent(expandable); };
+        });
+    }
+
+
+    
+
+    
+
+    // send height of page to parent
+    height += document.body.scrollHeight;
+    sendPageData();
+};
+
+window.onresize = function() {
+    if (collapsableCollapsed) {
+        height = document.body.scrollHeight;
+        sendPageData();
+    }
+}
+
+function sendPageData() {
+    let data = new Object();
+    data.type = "pageData";
+    data.height = height;
+    let path = window.location.pathname;
+    let page = path.split("/").pop().split(".")[0];
+    data.id = page;
+
+    sendMessage(data);
+}
+
+function sendMessage(mess) {
+    let origin;
+    if (window.location.protocol === "file:")    // because everything about webdev and security checks changes if you're testing locally ugh
+    {
+        origin = "*";
+    } else {
+        origin = "https://" + window.parent.location.hostname + window.parent.location.pathname;
+    }
+
+    //console.log(origin);
+
+    if (origin.startsWith("https://spookig.github.io") || origin === "*") // validate that the message is being sent to a trusted domain
+    {
+        parent.postMessage(mess, origin); 
+    }
+}
+
+
+
+function collapse(toggleElement) {
+    toggleElement.classList.toggle("active");
+    var content = toggleElement.nextElementSibling;
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+        height -= content.scrollHeight;
+
+        parentResize(content, -content.scrollHeight);
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        height += content.scrollHeight;
+
+        parentResize(content, content.scrollHeight);
+    }
+
+    collapsableCollapsed = false;
+    setTimeout(function() { collapsableCollapsed = true; }, 200);
+    sendPageData();
+}
+
+function parentResize(content, heightDifference) {
+    let parentContainer = content.parentElement.closest(".collapsable-content");
+    if (parentContainer != null) {
+        let oldHeight = 0;
+        if (parentContainer.style.maxHeight != "") {
+            oldHeight = parseInt(parentContainer.style.maxHeight);
+        }
+
+        parentContainer.style.maxHeight = (oldHeight + heightDifference) + "px";
+        //console.log(heightDifference);
+        //console.log(oldHeight + " + " + heightDifference + " = " + parentContainer.style.maxHeight);
+
+        //height += heightDifference;
+        parentResize(parentContainer, heightDifference);
+    }
+}
+
+
+
+
+function displayImage(image) {
+    let modal = document.getElementById("myModal");
+    let modalImg = document.getElementById("modalImage");
+    let captionText = document.getElementById("caption");
+
+    modal.onclick = hideImage;
+    modal.style.display = "block";
+    modalImg.src = image.src;
+    captionText.innerHTML = image.alt;
+}
+
+function sendImageToParent(image) {
+    let data = new Object();
+    data.type = "expandImage";
+    data.src = image.src;
+    data.alt = image.alt;
+
+    sendMessage(data);
+}
+
+function hideImage() {
+    let modal = document.getElementById("myModal");
+    modal.style.display = "none";
+    modal.onclick = null;
+}
